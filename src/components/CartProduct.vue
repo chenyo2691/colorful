1a8ecfa7c07076696fdaae776368a4629b2ee05e
chenyo2691 <357912616@qq.com>
2018-04-10 01:20:23 +0800

    购物车组件

--- src/components/CartProduct.vue
+++ src/components/CartProduct.vue
@@ -1,9 +1,9 @@
 <template>
     <f7-list>
-        <li class="swipeout swipeout-transitioning" v-for="(item,index) in productList" :key="index">
-            <div class="swipeout-content">
+        <li v-bind:class="[isEdit ? 'swipeout swipeout-transitioning' : '']" v-for="(item,index) in productList" :key="index">
+            <div v-bind:class="[isEdit ? 'swipeout-content' : '']">
                 <div class="item-content">
-                    <f7-checkbox style="padding-right:16px;" :checked="item.isCheck" @change="itemCheck($event, index)"></f7-checkbox>
+                    <f7-checkbox v-if="isEdit" style="padding-right:16px;" :checked="item.isCheck" @change="itemCheck($event, index)"></f7-checkbox>
                     <div class="item-media">
                         <img :src="item.productImageName.primary.img" width="80" />
                     </div>
@@ -40,35 +40,86 @@
                             <div class="item-cell">
                                 <div>
                                     <span class="c-price">{{`HK$${item.purchasePrice}`}}</span>
-                                    <CStepper style="float:right" v-model="item.purchaseQuantity" :min="0" :max="100" @change="quantityChange($event,index)"></CStepper>
+                                    <CStepper v-show="isEdit" style="float:right" v-model="item.purchaseQuantity" :min="0" :max="100" @change="quantityChange($event,index)"></CStepper>
+                                    <span v-show="!isEdit" style="float:right">x {{item.purchaseQuantity}}</span>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <f7-swipeout-actions right>
+            <f7-swipeout-actions right v-if="isEdit">
                 <f7-swipeout-button color="red" @click="deletePro(index)">删除</f7-swipeout-button>
             </f7-swipeout-actions>
         </li>
     </f7-list>
 </template>
 <script>
+import CStepper from '@/components/CStepper.vue';
+let dateFormat = function (date, fmt) {
+    let o = {
+        "M+": date.getMonth() + 1,                 //月份 
+        "d+": date.getDate(),                    //日 
+        "h+": date.getHours(),                   //小时 
+        "m+": date.getMinutes(),                 //分 
+        "s+": date.getSeconds(),                 //秒 
+        "q+": Math.floor((date.getMonth() + 3) / 3), //季度 
+        "S": date.getMilliseconds()             //毫秒 
+    };
+    if (/(y+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+    }
+    for (let k in o) {
+        if (new RegExp("(" + k + ")").test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        }
+    }
+    return fmt;
+};
 export default {
     props: [
-        'productList'
+        'productList',
+        'isEdit'
     ],
+    components: {
+        CStepper
+    },
+    filters: {
+        dfm: function (value) {
+            return dateFormat(new Date(value), 'yyyy-MM-dd');
+        }
+    },
     data() {
         return {
         };
     },
     methods: {
         deletePro(index) {
-
-        }
+            this.$emit('deletePro', index);
+        },
+        itemCheck($event, index) {
+            this.$emit('itemCheck', $event, index);
+        },
+        quantityChange($val, index) {
+            this.$emit('quantityChange', $val, index);
+        },
     }
 }
 </script>
-<style lang="css" scoped>
+<style lang="less" scoped>
+.c-ellipsis {
+  position: relative;
+  overflow: hidden;
+  white-space: nowrap;
+  max-width: 100%;
+  text-overflow: ellipsis;
+  &.c-text {
+    font-size: 12px;
+  }
+}
 
+.c-price {
+  color: red;
+  font-size: 14px;
+}
 </style>
--- src/pages/cart/List.vue
+++ src/pages/cart/List.vue
@@ -1,6 +1,6 @@
 <template>
     <f7-page class="cart-container">
-        <f7-navbar v-show="currentStep===0">
+        <f7-navbar v-if="currentStep===0">
             <f7-nav-left>
                 <f7-link back class="nav-icon">
                     <i class="fa fa-chevron-left"></i>
@@ -22,8 +22,7 @@
                 </f7-link>
             </f7-nav-right>
         </f7-navbar>
-
-        <f7-navbar v-show="currentStep===1">
+        <f7-navbar v-if="currentStep===1">
             <f7-nav-left>
                 <f7-link class="nav-icon" @click="currentStep--">
                     <i class="fa fa-chevron-left">确认订单</i>
@@ -32,59 +31,7 @@
         </f7-navbar>
 
         <div v-show="currentStep===0">
-            <f7-list>
-                <li class="swipeout swipeout-transitioning" v-for="(item,index) in productList" :key="index">
-                    <div class="swipeout-content">
-                        <div class="item-content">
-                            <f7-checkbox style="padding-right:16px;" :checked="item.isCheck" @change="itemCheck($event, index)"></f7-checkbox>
-                            <div class="item-media">
-                                <img :src="item.productImageName.primary.img" width="80" />
-                            </div>
-                            <div class="item-inner item-cell">
-                                <div class="item-row">
-                                    <div class="item-cell">
-                                        <div class="c-ellipsis">
-                                            {{item.productName}}
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="item-row">
-                                    <div class="item-cell">
-                                        <div class="c-ellipsis c-text">
-                                            {{item.productNumber}}
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="item-row">
-                                    <div class="item-cell">
-                                        <div class="c-ellipsis c-text">
-                                            {{item.productSpecification}}
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="item-row">
-                                    <div class="item-cell">
-                                        <div class="c-ellipsis c-text">
-                                            {{item.deliveryDay | dfm}}
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="item-row">
-                                    <div class="item-cell">
-                                        <div>
-                                            <span class="c-price">{{`HK$${item.purchasePrice}`}}</span>
-                                            <CStepper style="float:right" v-model="item.purchaseQuantity" :min="0" :max="100" @change="quantityChange($event,index)"></CStepper>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <f7-swipeout-actions right>
-                        <f7-swipeout-button color="red" @click="deletePro(index)">删除</f7-swipeout-button>
-                    </f7-swipeout-actions>
-                </li>
-            </f7-list>
+            <CartProduct :productList="productList" :isEdit="true" @deletePro="deletePro" @itemCheck="itemCheck" @quantityChange="quantityChange"></CartProduct>
         </div>
         <div v-show="currentStep===1">
             <!-- 提货方式 -->
@@ -107,100 +54,34 @@
                 </f7-list-item>
             </f7-list>
 
-            <f7-list>
-                <li v-for="(item,index) in productList" :key="index" v-if="item.isCheck">
-                    <div class="item-content">
-                        <div class="item-media">
-                            <img :src="item.productImageName.primary.img" width="80" />
-                        </div>
-                        <div class="item-inner item-cell">
-                            <div class="item-row">
-                                <div class="item-cell">
-                                    <div class="c-ellipsis">
-                                        {{item.productName}}
-                                    </div>
-                                </div>
-                                <div class="item-cell">
-                                    <div class="c-ellipsis">
-                                        {{item.productNumber}}
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="item-row">
-                                <div class="item-cell">
-                                    <div class="c-ellipsis">
-                                        {{item.productSpecification}}
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="item-row">
-                                <div class="item-cell">
-                                    <div class="c-ellipsis">
-                                        {{item.deliveryDay | dfm}}
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="item-row">
-                                <div class="item-cell">
-                                    <div>
-                                        <span style="color:red">{{`HK$${item.purchasePrice}`}}</span>
-                                        <span style="float:right">x {{item.purchaseQuantity}}</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </li>
-            </f7-list>
-
+            <CartProduct :productList="productChecked" :isEdit="false"></CartProduct>
         </div>
 
         <f7-toolbar bottom-md>
+            <!-- step1 -->
             <span style="padding-left:16px" v-show="currentStep===0">
                 <label>
                     <f7-checkbox :checked="selectAll" @change="handleCheckAll"></f7-checkbox>全部</label>
             </span>
             <span v-show="currentStep===0">合计：{{totalValue}}元</span>
-            <div class="orderSubmitBtn" @click="submitorder" v-show="currentStep===0">去结算({{productCheckLength}})</div>
-
+            <div class="orderSubmitBtn" @click="++currentStep" v-show="currentStep===0">去结算({{productCheckLength}})</div>
+            <!-- step2 -->
             <span v-show="currentStep===1"></span>
             <span v-show="currentStep===1">实付款：HK$4343.00</span>
-            <div class="orderSubmitBtn" v-show="currentStep===1">提交订单</div>
+            <div class="orderSubmitBtn" v-show="currentStep===1" @click="submitorder">提交订单</div>
         </f7-toolbar>
     </f7-page>
 </template>
 <script>
 import api from '@/api/index.js';
 import storage from '@/utils/xStorage.js';
-import CStepper from '@/components/CStepper.vue';
-let dateFormat = function (date, fmt) {
-    let o = {
-        "M+": date.getMonth() + 1,                 //月份 
-        "d+": date.getDate(),                    //日 
-        "h+": date.getHours(),                   //小时 
-        "m+": date.getMinutes(),                 //分 
-        "s+": date.getSeconds(),                 //秒 
-        "q+": Math.floor((date.getMonth() + 3) / 3), //季度 
-        "S": date.getMilliseconds()             //毫秒 
-    };
-    if (/(y+)/.test(fmt)) {
-        fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
-    }
-    for (let k in o) {
-        if (new RegExp("(" + k + ")").test(fmt)) {
-            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-        }
-    }
-    return fmt;
-};
+// import CStepper from '@/components/CStepper.vue';
+import CartProduct from '@/components/CartProduct.vue';
+
 export default {
     components: {
-        CStepper
-    },
-    filters: {
-        dfm: function (value) {
-            return dateFormat(new Date(value), 'yyyy-MM-dd');
-        }
+        // CStepper,
+        CartProduct
     },
     mounted() {
         this.init();
@@ -210,6 +91,12 @@ export default {
         productCheckLength: function () {
             let checkedList = this.productList.filter(v => v.isCheck);
             return checkedList.length;
+        },
+        productChecked: function () {
+            // let productList = this.productList;
+            return this.productList.filter(function (product) {
+                return product.isCheck;
+            });
         }
     },
     data() {
@@ -280,20 +167,20 @@ export default {
                 }.bind(this)
             );
         },
+        itemCheck($event, index) {
+            this.productList[index].isCheck = $event.target.checked;
+            let checkedList = this.productList.filter(v => v.isCheck);
+            this.selectAll = checkedList.length === this.productList.length;
+        },
+        // 全选
         handleCheckAll($event) {
             this.selectAll = $event.target.checked;
             this.productList.forEach(function (element) {
                 element.isCheck = this.selectAll;
             }, this);
         },
-        itemCheck($event, index) {
-            this.productList[index].isCheck = $event.target.checked;
-            let checkedList = this.productList.filter(v => v.isCheck);
-            this.selectAll = checkedList.length === this.productList.length;
-        },
         submitorder() {
             console.log('提交订单');
-            this.currentStep++;
         }
     }
 }
@@ -311,22 +198,6 @@ export default {
     display: flex;
     align-items: center;
     justify-content: center;
-  }
-
-  .c-ellipsis {
-    position: relative;
-    overflow: hidden;
-    white-space: nowrap;
-    max-width: 100%;
-    text-overflow: ellipsis;
-    &.c-text {
-      font-size: 12px;
-    }
-  }
-
-  .c-price {
-    color: red;
-    font-size: 14px;
   }
 }
 </style>
